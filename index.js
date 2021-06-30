8b40bd33e76416010030e031e30575d37d059e38
hasannjan <hasanjan2000@gmail.com>
2021-06-30 23:28:23 +0500

    JS

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert("Hello World!");
-alert("Again");
+for (var i = 0; i < 5; i++) {
+    
+    for (var j = 0; j <= i; j++) {
+      document.write("*");
+    //   if (j > i + 1) document.write("&nbsp;");
+    }
+    document.write('<br/>')
+  }
