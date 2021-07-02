36ebc2e1193986a2ac7ec6b83020d9090d8c43a3
hasannjan <hasanjan2000@gmail.com>
2021-07-02 23:40:01 +0500

    Discard

--- index.js
+++
@@ -1,8 +0,0 @@
-for (var i = 0; i < 5; i++) {
-    
-    for (var j = 0; j <= i; j++) {
-      document.write("*");
-    //   if (j > i + 1) document.write("&nbsp;");
-    }
-    document.write('<br/>')
-  }
